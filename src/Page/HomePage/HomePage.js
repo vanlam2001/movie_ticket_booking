bac23b918956719442d13962b14ed6e4a1d7e959
longdv ph29867 <emchuaroinhe@gmail.com>
2023-04-10 21:21:11 +0700

    update css

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner'
 import Header from '../../Components/Header/Header'
-import ListMovieHome from '../../Components/ListMovieHome/ListMovieHome'
-import TabsMovieHome from '../../Components/TabsMovieHome/TabsMovieHome'
 
 
 export default function HomePage() {
@@ -10,7 +8,6 @@ export default function HomePage() {
         <div>
             <Header></Header>
             <Banner></Banner>
-            <TabsMovieHome/>
         </div>
     )
 }
