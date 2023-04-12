232c2fa2e4f5fc5713e12299e21b1a687fa4bec5
vanlam <vanlam1412@gmail.com>
2023-04-12 16:03:54 +0700

    update

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,18 +1,6 @@
-
-import axios from "axios";
-import { BASE_URL, configHeaders } from "./config"
+import { https } from "./config"
 export const userServ = {
-    login: (loginData) => {
-
-        return axios({
-            url: `${BASE_URL}/api/QuanLyNguoiDung/DangNhap`,
-            method: "POST",
-            data: loginData,
-            headers: configHeaders(),
-        });
-    },
+    login: (login) => {
+        return https.post('/api/QuanLyNguoiDung/DangNhap', login)
+    }
 };
-
-userServ.login();
-
-
