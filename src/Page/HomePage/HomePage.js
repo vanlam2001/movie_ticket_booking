2192caab55eac39805ff0ff104b29d786bdeb97d
vanlam <vanlam1412@gmail.com>
2023-04-12 16:13:55 +0700

    update

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner'
 import Header from '../../Components/Header/Header'
+import TabsMovieHome from '../../Components/TabsMovieHome/TabsMovieHome'
 
 
 export default function HomePage() {
@@ -8,6 +9,7 @@ export default function HomePage() {
         <div>
             <Header></Header>
             <Banner></Banner>
+            <TabsMovieHome></TabsMovieHome>
         </div>
     )
 }
