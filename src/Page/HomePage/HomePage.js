35a0404b3bd9a4b747cd27ce823f35ea1f856896
longdv ph29867 <emchuaroinhe@gmail.com>
2023-04-12 17:31:50 +0700

    up footer

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner'
-import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
-import ListMovieHome from '../../Components/ListMovieHome/ListMovieHome'
 
 
 export default function HomePage() {
@@ -10,7 +8,6 @@ export default function HomePage() {
         <div>
             <Header></Header>
             <Banner></Banner>
-            <Footer/>
         </div>
     )
 }
