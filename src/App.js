e926d538b37178f000c89c40118ebe5c4995c7a6
vanlam <vanlam1412@gmail.com>
2023-04-15 15:10:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import BookTicketPage from './Page/BookTicketPage/BookTicketPage';
 import HomePage from './Page/HomePage/HomePage';
 import LoginPage from './Page/LoginPage/LoginPage';
 import Register from './Page/Register/Register';
@@ -14,7 +13,7 @@ function App() {
           <Route path='/' element={<Layout Component={HomePage} />}></Route>
           <Route path='/login' element={<LoginPage></LoginPage>}></Route>
 
-          <Route path='/booking/:id' element={<Layout Component={BookTicketPage} />}></Route>
+
 
           <Route path='/register' element={<Register></Register>}></Route>
 
