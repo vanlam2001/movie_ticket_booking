da6fe3f4bc3e96d7fabd17d15bc6c162a19c712f
vanlam <vanlam1412@gmail.com>
2023-04-20 21:07:23 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   mode: 'jit',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    require("tw-elements/dist/plugin.cjs")
   ],
+  darkMode: "class"
 }
